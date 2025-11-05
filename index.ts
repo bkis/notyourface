d331348071f6bcaef849749038fd8f3627497bf6
bkis <kiss@mailbox.org>
2025-11-05 13:16:48 +0100

    Do a general conceptual overhaul

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 import nyf from './src/main.ts';
 
-export const getImgElements = (seeds: string[]) =>
-  seeds.map((seed) => nyf.imgEl({ seed, scale: 128 }));
+export const getImgElement = (
+  seed?: string,
+  palette?: string[],
+  size?: number,
+  noCache?: boolean
+) => nyf.imgEl({ seed, palette, noCache, size });
--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,162 @@
 const _cache: Map<string, string> = new Map();
 
+/**
+ * TYPES
+ */
+
+type Modify<T, R> = Omit<T, keyof R> & R;
+
 interface NYFOptions {
   seed?: unknown;
-  scale?: number;
+  size?: number;
   palette?: string[];
   noCache?: boolean;
   maxCacheSize?: number;
 }
 
-interface GuaranteedNYFOptions extends Required<NYFOptions> {
-  seed: string;
-}
+type GuaranteedNYFOptions = Modify<
+  Required<NYFOptions>,
+  {
+    seed: string;
+    palette?: string[];
+    rnd: () => number;
+  }
+>;
+
+/**
+ * INTERNAL UTILITY FUNCTIONS
+ */
 
 const _seedInt = (input: unknown) => {
   let str: string;
   try {
-    str = JSON.stringify(input);
+    str = JSON.stringify(input ?? Math.random().toString());
   } catch {
     str = String(input);
   }
   let hash = 0;
-  for (let i = 0, len = str.length; i < len; i++) {
-    hash = (hash << 5) - hash + str.charCodeAt(i);
+  for (const char of str) {
+    hash = (hash << 5) - hash + char.charCodeAt(0);
     hash |= 0;
   }
-  return Math.abs(hash >>> 0);
+  return hash;
 };
 
 const _seedStr = (input: unknown) => {
   return _seedInt(input).toString().padStart(8, '0');
 };
 
-const _getInt = (min: number = 0, max: number = 100, seed: string) => {
-  if (max <= min) throw Error(`Invalid arguments for min (${min}) and max (${max}).`);
-  return Math.round(_seedInt(seed) % (max + 1 - min)) + min;
+function _getRndFn(a: number) {
+  return function () {
+    let t = (a += 0x6d2b79f5);
+    t = Math.imul(t ^ (t >>> 15), t | 1);
+    t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+    return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+  };
+}
+
+const _getInt = (min: number = 0, max: number = 100, rnd: () => number) => {
+  if (max < min) throw Error(`Invalid arguments for min (${min}) and max (${max}).`);
+  const span = Math.floor(max) + 1 - Math.ceil(min);
+  return Math.floor(span * rnd()) + Math.ceil(min);
 };
 
-const _pick = <T>(arr: T[], seed: string) => {
-  return arr[_getInt(0, arr.length - 1, seed)];
+const _pick = <T>(arr: T[], rnd: () => number) => {
+  return arr[_getInt(0, arr.length - 1, rnd)];
 };
 
-const _getCol = (seed?: string) => {
-  if (seed) {
-    return '#' + (_seedInt(seed) % 16777215).toString(16);
+const _getCol = (rnd: () => number, palette?: string[]) => {
+  if (palette?.length) {
+    return _pick(palette, rnd);
   } else {
-    return '#' + (Math.random() * 16777215).toString(16);
+    return (
+      '#' +
+      _getInt(0, 0xfffff * 1000000, rnd)
+        .toString(16)
+        .slice(0, 6)
+    );
   }
-};
-
-const _generate = (o: GuaranteedNYFOptions) => {
-  const canvas = document.createElement('canvas');
-  canvas.width = o.scale;
-  canvas.height = o.scale;
-  const ctx = canvas.getContext('2d');
-  if (!ctx) throw Error('Could not get canvas context.');
-  // background
-  ctx.fillStyle = _pick(o.palette, o.seed + 'bg');
-  ctx.fillRect(0, 0, o.scale, o.scale);
-  // some square
-  ctx.fillStyle = _pick(o.palette, o.seed + 's1');
-  const s1scale = _getInt(o.scale * 0.2, o.scale * 0.8, o.seed + 's1s');
-  ctx.fillRect(
-    _getInt(0 - o.scale * 0.2, o.scale * 0.8, o.seed + 's1x'),
-    _getInt(0 - o.scale * 0.2, o.scale * 0.8, o.seed + 's1y'),
-    s1scale,
-    s1scale
-  );
-  // some circle
-  ctx.beginPath();
-  ctx.arc(
-    _getInt(0 - o.scale * 0.2, o.scale * 0.8, o.seed + 'c1x'),
-    _getInt(0 - o.scale * 0.2, o.scale * 0.8, o.seed + 'c1y'),
-    _getInt(o.scale * 0.1, o.scale * 0.6, o.seed + 'c1r'),
-    0,
-    2 * Math.PI
-  );
-  ctx.fillStyle = _pick(o.palette, o.seed + 'c1');
-  ctx.fill();
-  return canvas.toDataURL('image/png');
 };
 
 const _getOptions = (o?: NYFOptions) => {
   const seed = _seedStr(o?.seed ?? Math.random().toString());
   return {
     seed,
-    scale: o?.scale ?? 256,
-    palette: o?.palette ?? Array.from(Array(5).keys()).map((k) => _getCol(k + seed + k)),
+    size: o?.size ?? 128,
     noCache: o?.noCache ?? false,
     maxCacheSize: o?.maxCacheSize ?? 1024,
+    palette: o?.palette,
+    rnd: _getRndFn(_seedInt(seed)),
   } as GuaranteedNYFOptions;
 };
+
+const _drawSquare = (
+  ctx: CanvasRenderingContext2D,
+  o: GuaranteedNYFOptions,
+  sizeMod: number = 1
+) => {
+  ctx.fillStyle = _getCol(o.rnd, o.palette);
+  const size = _getInt(o.size * 0.5 * sizeMod, o.size * sizeMod, o.rnd);
+  const posMod = size / 2;
+  ctx.fillRect(
+    _getInt(o.size * 0.2 - posMod, o.size - o.size * 0.2 + posMod, o.rnd),
+    _getInt(o.size * 0.2 - posMod, o.size - o.size * 0.2 + posMod, o.rnd),
+    size,
+    size
+  );
+};
+
+const _drawCircle = (
+  ctx: CanvasRenderingContext2D,
+  o: GuaranteedNYFOptions,
+  sizeMod: number = 1
+) => {
+  ctx.beginPath();
+  const radius = _getInt(o.size * 0.5 * sizeMod, o.size * sizeMod, o.rnd);
+  const posMod = radius / 2;
+  ctx.arc(
+    _getInt(o.size * 0.2 - posMod, o.size - o.size * 0.2 + posMod, o.rnd),
+    _getInt(o.size * 0.2 - posMod, o.size - o.size * 0.2 + posMod, o.rnd),
+    radius,
+    0,
+    2 * Math.PI
+  );
+  ctx.fillStyle = _getCol(o.rnd, o.palette);
+  ctx.fill();
+};
+
+/**
+ * AVATAR GENERATION PROCEDURE
+ */
+
+const _generate = (o: GuaranteedNYFOptions) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = o.size;
+  canvas.height = o.size;
+  const ctx = canvas.getContext('2d');
+  if (!ctx) throw Error('Could not get canvas context.');
+  // background
+  ctx.fillStyle = _getCol(o.rnd, o.palette);
+  ctx.fillRect(0, 0, o.size, o.size);
+  _drawSquare(ctx, o, 1.0); // some square
+  _drawCircle(ctx, o, 0.8); // some circle
+  _drawSquare(ctx, o, 0.6); // some square
+  _drawCircle(ctx, o, 0.4); // some circle
+  return canvas.toDataURL('image/png');
+};
+
+/**
+ * EXTERNAL API
+ */
+
 const nyf = {
   dataURL(options?: NYFOptions): string {
     // populate options object to work with
     const o = _getOptions(options);
+    if (o.size > 256)
+      console.warn(
+        `Be careful with the size of your avatars. Due to performance reasons, the maximum recommended size is 256px.`
+      );
     // process seed to get something that makes sense as a mapping key
     // check if we can serve from cache (if cache usage isn't disabled in options)
     if (!o.noCache && _cache.has(o.seed)) {
